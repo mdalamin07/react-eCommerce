2a83dd54a6dcdf7fc5d13c64395855827a53a0c2
MD.AL-AMIN <mdalmain67378@gmail.com>
2022-12-21 01:15:28 +0600

    create soring data option

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -4,7 +4,7 @@ import { BsFillGridFill, BsList } from "react-icons/bs"
 import { useFilterContext } from '../context/filter_context';
 
 const Sort = () => {
-  const {filter_products, grid_view, setGridView, setListView } = useFilterContext();
+  const {filter_products, grid_view, setGridView, setListView, sorting } = useFilterContext();
   return (
     <Wrapper className="sort-section">
       <div className="sorting-list--grid">
@@ -24,7 +24,23 @@ const Sort = () => {
       <div className="product-data">
         <p>{`${filter_products.length}`} Product Available</p>
       </div>
-      <div className="sort-selection">sort-selection</div>
+      <div className="sort-selection">
+        <form action="#">
+          <label htmlFor="sort"></label>
+          <select 
+            name="sort" id="sort" 
+            className="sort-selection--style" 
+            onClick={sorting}>
+            <option value="lowest">Price(lowest)</option>
+            <option value="#" disabled></option>
+            <option value="highest">Price(heighest)</option>
+            <option value="#" disabled></option>
+            <option value="a-z">Price(a-z)</option>
+            <option value="#" disabled></option>
+            <option value="z-a">Price(z-a)</option>
+          </select>
+        </form>
+      </div>
     </Wrapper>
   )
 };
--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -9,6 +9,7 @@ const initialState = {
     filter_products: [],
     all_products: [],
     grid_view: true,
+    sorting_value: "lowest",
 }
 
 export const FilterContextProvider = ({children}) => {
@@ -18,20 +19,31 @@ export const FilterContextProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     // To set grid view
-     const setGridView = () => {
-        return dispatch({type: "SET_GRID_VIEW"});
-     }
+    const setGridView = () => {
+    return dispatch({type: "SET_GRID_VIEW"});
+    };
+
     // To set list view
-     const setListView = () => {
-        return dispatch({type: "SET_LIST_VIEW"});
-     }
+    const setListView = () => {
+    return dispatch({type: "SET_LIST_VIEW"});
+    };
+
+    //  sorting function
+    const sorting = (event) => {
+        let userValue = event.target.value;
+        dispatch({type : "GET_SORT_VALUE", payload: userValue});
+    };
+    // to sort the products
+    useEffect(() => {
+        dispatch({ type: "SORTING_PRODUCTS" });
+    },[products, state.sorting_value]);
 
     useEffect(() => {
         dispatch({type: "LOAD_FILTER_PRODUCTS", payload: products });
     },[products]);
 
     return (
-        <FilterContext.Provider value={{...state, setGridView, setListView }}>
+        <FilterContext.Provider value={{...state, setGridView, setListView, sorting }}>
             {children}
         </FilterContext.Provider>
     )

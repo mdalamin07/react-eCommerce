523a8c368f0eda98a5083ecd3b132a0f9f34847b
MD.AL-AMIN <mdalmain67378@gmail.com>
2022-12-26 00:37:50 +0600

    compleate filter section part in products page

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -10,6 +10,15 @@ const initialState = {
     all_products: [],
     grid_view: true,
     sorting_value: "lowest",
+    filters : {
+        text: "",
+        category: "all",
+        company: "all",
+        color: "all",
+        maxPrice: 0,
+        price: 0,
+        minPrice: 0,
+    }
 }
 
 export const FilterContextProvider = ({children}) => {
@@ -33,17 +42,37 @@ export const FilterContextProvider = ({children}) => {
         let userValue = event.target.value;
         dispatch({type : "GET_SORT_VALUE", payload: userValue});
     };
+    // update the filter value
+    const updateFilterValue = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+
+        return dispatch({ type: "UPDATE_FILTERS_VALUE", payload: { name, value }});
+    };
+    // To clear the filter
+    const clearFilters = () => {
+        dispatch({ type: "CLEAR_FILTERS"});
+    };
     // to sort the products
     useEffect(() => {
+        dispatch({ type: "FILTER_PRODUCTS"});
         dispatch({ type: "SORTING_PRODUCTS" });
-    },[products, state.sorting_value]);
-
+    },[products, state.sorting_value, state.filters]);
+    // to liad all products for grid and list view
     useEffect(() => {
         dispatch({type: "LOAD_FILTER_PRODUCTS", payload: products });
     },[products]);
 
     return (
-        <FilterContext.Provider value={{...state, setGridView, setListView, sorting }}>
+        <FilterContext.Provider 
+            value={{
+                ...state, 
+                setGridView, 
+                setListView, 
+                sorting, 
+                updateFilterValue, 
+                clearFilters, 
+            }}>
             {children}
         </FilterContext.Provider>
     )
--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -2,10 +2,13 @@ const filterReducer = (state, action) => {
 
     switch(action.type) {
         case "LOAD_FILTER_PRODUCTS" :
+            let priceArr = action.payload.map((curElem) => curElem.price);
+            let maxPrice = Math.max(...priceArr);
             return {
                 ...state,
                 filter_products : [...action.payload],
                 all_products : [...action.payload],
+                filters: {...state.filters, maxPrice, price: maxPrice},
             }
         case "SET_GRID_VIEW" :
             return {
@@ -51,6 +54,67 @@ const filterReducer = (state, action) => {
                 ...state,
                 filter_products: newSortData,
             };  
+        case "UPDATE_FILTERS_VALUE":
+            const { name, value } = action.payload;
+
+            return {
+                ...state,
+                filters: {
+                    ...state.filters,
+                    [name]: value,
+                }
+            }
+        case "FILTER_PRODUCTS":
+            let { all_products } = state;
+            let tempFilterProducts = [...all_products];
+
+            const { text, category, company, color, price } = state.filters;
+
+            if(text) {
+                tempFilterProducts = tempFilterProducts.filter((curElem) => {
+                    return curElem.name.toLowerCase().includes(text);
+                });
+            }
+            if(category !== "all") {
+                tempFilterProducts = tempFilterProducts.filter((curElem) => 
+                    curElem.category === category
+                );
+            }
+            if(company !== "all") {
+                tempFilterProducts = tempFilterProducts.filter((curElem) => 
+                    curElem.company.toLowerCase() === company.toLowerCase()
+                );
+            }
+            if(color !== "all") {
+                tempFilterProducts = tempFilterProducts.filter((curElem) => 
+                    curElem.colors.includes(color)
+                );
+            }
+            if(price === 0) {
+                tempFilterProducts = tempFilterProducts.filter((curElem) =>        curElem.price === price
+                );
+            }else {
+                tempFilterProducts = tempFilterProducts.filter((curElem) =>        curElem.price <= price
+                );
+            }
+            return {
+                ...state,
+                filter_products: tempFilterProducts,
+            };
+            case "CLEAR_FILTERS": 
+                return {
+                    ...state,
+                    filters: {
+                        ...state.filters,
+                        text: "",
+                        category: "all",
+                        company: "all",
+                        color: "all",
+                        maxPrice: state.filters.maxPrice,
+                        price: state.filters.maxPrice,
+                        minPrice: state.filters.maxPrice,
+                    },
+                };
         default : return state;
     }
 }
